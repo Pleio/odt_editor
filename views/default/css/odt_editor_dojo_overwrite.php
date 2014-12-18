9a9a5e41a9277e3a30e2198657e21097797bb6c6
Friedrich W. H. Kossebau <friedrich@kogmbh.com>
2014-12-18 16:40:19 +0100

    Also fix annotationbutton being square

--- views/default/css/odt_editor_dojo_overwrite.php
+++ views/default/css/odt_editor_dojo_overwrite.php
@@ -2,11 +2,20 @@
 ?>
 @namespace office url(urn:oasis:names:tc:opendocument:xmlns:office:1.0);
 
+/* dojo sets margin-top to 1em, which screws the elgg header */
 .elgg-inner > h1 {
     margin-top: 0px
 }
 
+/* elgg set body to have background-image */
 office|body {
     background-color: white;
     background-image: none;
+}
+
+/* for some yet to be researched reason elgg's rule ".developers-log *" also affects border-radius this, so enforce it */
+.annotationRemoveButton {
+    -webkit-border-radius: 10px !important;
+    -moz-border-radius: 10px !important;
+    border-radius: 10px !important;
 }
