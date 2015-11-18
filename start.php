e96ddf873aa51a1dc43aa182458420d7fcc93548
Bart Jeukendrup <bart@jeukendrup.nl>
2015-11-18 14:54:12 +0100

    fixed: only show menu when file_tools is enabled

--- start.php
+++ start.php
@@ -28,7 +28,9 @@ function odt_editor_init() {
     elgg_register_plugin_hook_handler("route", "file", "odt_editor_route_file_handler");
     
     // extend file page menu
-    elgg_register_plugin_hook_handler("register", "menu:title", "odt_editor_file_menu_title_hook");
+    if (elgg_is_active_plugin('file_tools')) {
+        elgg_register_plugin_hook_handler("register", "menu:title", "odt_editor_file_menu_title_hook");
+    }
 
     elgg_register_action("odt_editor/upload", elgg_get_plugins_path() . "odt_editor/actions/odt_editor/upload.php");
     elgg_register_action("odt_editor/upload_asnew", elgg_get_plugins_path() . "odt_editor/actions/odt_editor/upload_asnew.php");
